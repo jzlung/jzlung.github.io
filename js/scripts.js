dc1c0d428a3c61b608969693322133cd10c4ab95
Jerry Lung <jlung@Jerrys-MacBook-Pro-3.local>
2015-01-12 13:17:57 -0800

    new webpage up at test

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/* SCRIPTS.JS for Jerry Lung's website <www.jerrylung.com> */
+
+// TODO: make the scrollTop() a percentage
+$(window).scroll(function(){
+	console.log($(document).scrollTop());
+  if ($(document).scrollTop() <= 200) {
+    $('#main').removeClass('invisible');
+  } else {
+    $('#main').addClass('invisible');
+  }
+});
+
+
+<script type="text/javascript">
+function changePic() {
+  document.getElementById("profilePic").src = "resources/kubrick.jpg";
+}
+
+function normalPic() {
+  document.getElementById("profilePic").src = "resources/dinojerr.jpg";
+}
+</script>
