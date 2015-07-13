0b0548f74e6197b9905ab16d21b9c7ba8c5eb28f
Jerry Lung <jlung@Jerrys-MacBook-Pro-3.local>
2015-07-13 14:38:31 -0700

    fixed scroll to be by percentage, more extensible

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,38 @@
 /* SCRIPTS.JS for Jerry Lung's website <www.jerrylung.com> */
 
-// TODO: make the scrollTop() a percentage
-$(window).scroll(function(){
-	console.log($(document).scrollTop());
-  if ($(document).scrollTop() <= 200) {
-    $('#main').removeClass('invisible');
-    $('#projects').addClass('invisible');
-  } else {
-    $('#main').addClass('invisible');
-  } 
+var numSections = 3;
+var winHeight = $(window).height();
+var docHeight = $(document).height();
 
-  if ($(document).scrollTop() >= 450) {
+function scrollPercent(){
+  var scrollTop = $(document).scrollTop();
+  var scrollPercent = (scrollTop) / (docHeight - winHeight);
+  return scrollPercent;
+}
+
+// Set all divs to be invisible, revealed only by scrolls
+$('#projects').addClass('invisible');
+$('#contact').addClass('invisible');
+// $('#projects').addClass('invisible');
+
+// Start the page with a fade-in
+setTimeout(function() {
+  $('#main').removeClass('invisible');
+}, 500);
+
+$(window).scroll(function(){
+	// console.log($(document).scrollTop());
+  var scroll = scrollPercent();
+  console.log(scroll);
+
+  if (scroll >= 1/numSections) {
     $('#projects').removeClass('invisible');
   }
+
+  if (scroll >= 2/numSections) {
+    $('#contact').removeClass('invisible');
+  }
+
 });
 
 
